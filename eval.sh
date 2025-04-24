a4902a765f7ff262c526d54e4db374b5c3885d35
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2025-04-24 16:56:29 +0900

    parallel proc added

--- eval.sh
+++ eval.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 #SBATCH --time=6-0
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:3
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
-#SBATCH -w ariel-v9
+#SBATCH -w ariel-v13
 #SBATCH -o eval.out
 
 
-python eval_uvg.py --original_root data/UVG/ \
-                   --pred_root benchmark/unicontrol_gop8 \
+# GPU 0
+CUDA_VISIBLE_DEVICES=0 python eval_uvg.py --original_root data/UVG/ \
+                   --pred_root benchmark/unicontrol_gop8_grid9 \
                    --config configs/uni_v15_perco.yaml \
                    --ckpt ckpt/uni.ckpt \
                    --gop 8 \
-                   --resolution 1080p
+                   --resolution 1080p --grid 9
 
-python eval_uvg.py --original_root data/UVG/ \
-                   --pred_root benchmark/unicontrol_gop4 \
+# GPU 1
+CUDA_VISIBLE_DEVICES=1 python eval_uvg.py --original_root data/UVG/ \
+                   --pred_root benchmark/unicontrol_gop4_grid_9\
                    --config configs/uni_v15_perco.yaml \
                    --ckpt ckpt/uni.ckpt \
                    --gop 4 \
-                   --resolution 1080p
+                   --resolution 1080p --grid 9
 
-python eval_uvg.py --original_root data/UVG/ \
-                   --pred_root benchmark/unicontrol_gop16 \
+# GPU 2
+CUDA_VISIBLE_DEVICES=2 python eval_uvg.py --original_root data/UVG/ \
+                   --pred_root benchmark/unicontrol_gop16_grid9 \
                    --config configs/uni_v15_perco.yaml \
                    --ckpt ckpt/uni.ckpt \
                    --gop 16 \
-                   --resolution 1080p
+                   --resolution 1080p --grid 9
+
+wait
